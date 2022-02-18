8accd820e8b90a9f42b04ec90b03a27af1d146ec
Dhruv Panchal <dhruvpanchal1491@gmail.com>
2022-02-18 15:27:58 +0530

    problem 1512 number of good pairs

---
+++ python_solutions/notebooks/1512_number_of_good_pairs.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        good_pair_counter = 0
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j]:
+                    good_pair_counter = good_pair_counter + 1
+        return good_pair_counter
+
+
+if __name__ == '__main__':
+    nums = [1, 1, 1, 1]
+    obj = Solution()
+    obj.numIdenticalPairs(nums)
