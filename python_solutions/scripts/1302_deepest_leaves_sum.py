22054bf6329c2f4bc262e20d9726c7335260f13e
Dhruv Panchal <dhruvpanchal1491@gmail.com>
2022-03-02 23:32:26 +0530

    aesthetic changes

--- python_solutions/scripts/1302_deepest_leaves_sum.py
+++ python_solutions/scripts/1302_deepest_leaves_sum.py
@@ -1,10 +1,14 @@
 class Solution:
-    def deepestLeavesSum(self, root: TreeNode):
+    def deepestLeavesSum(self, root):
         sums = []
-        def dfs(node: TreeNode, lvl: int):
-            if lvl == len(sums): sums.append(node.val)
-            else: sums[lvl] += node.val
-            if node.left: dfs(node.left, lvl+1)
-            if node.right: dfs(node.right, lvl+1)
+        def dfs(node, lvl):
+            if lvl == len(sums):
+                sums.append(node.val)
+            else:
+                sums[lvl] += node.val
+            if node.left:
+                dfs(node.left, lvl+1)
+            if node.right:
+                dfs(node.right, lvl+1)
         dfs(root, 0)
         return sums[-1]
