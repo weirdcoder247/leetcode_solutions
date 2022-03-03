dfb1d0a687982aeead8d63c5a9abbaab8a821153
Dhruv Panchal <dhruvpanchal1491@gmail.com>
2022-03-03 20:21:23 +0530

    added solution for problem no. 1282

---
+++ python_solutions/scripts/1282_group_people_into_correct_group_size.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def groupThePeople(self, groupSizes):
+        """
+        :type groupSizes: List[int]
+        :rtype: List[List[int]]
+        """
+        n = len(groupSizes)
+        unique = set(groupSizes)
+        target = []
+        index = 0
+        group_switch = {}
+        for i in unique:
+            group_switch[i] = []
+        while(groupSizes != []):
+            group_switch[groupSizes.pop(0)].append(index)
+            index += 1
+        for i in unique:
+            if len(group_switch[i]) == i:
+                target.append(group_switch[i])
+                del group_switch[i]
+            else:
+                while(len(group_switch[i]) != 0):
+                    target.append(group_switch[i][-i:])
+                    del group_switch[i][-i:]
+
+        return target
+
+
+def main():
+    obj = Solution()
+    groupSizes = [3,3,3,3,3,1,3]
+
+    return obj.groupThePeople(groupSizes)
+
+if __name__ == "__main__":
+    print(main())
