b269b23571f1ed18b4e4bead1f869a9204a99f5b
Dhruv Panchal <dhruvpanchal1491@gmail.com>
2022-03-03 16:04:23 +0530

    added solution for goal parser problem

---
+++ python_solutions/scripts/1678_goal_parser_interpretation.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def interpret(self, command):
+        """
+        :type command: str
+        :rtype: str
+        """
+        target = ''
+        command = list(command)
+        curr_cmd = ''
+        while(command != []):
+            curr_cmd += command.pop(0)
+            if curr_cmd == 'G':
+                target += 'G'
+                curr_cmd = ''
+            elif curr_cmd == '()':
+                target += 'o'
+                curr_cmd = ''
+            elif curr_cmd == '(al)':
+                target +='al'
+                curr_cmd = ''
+        return target
+
+
+
+def main():
+    obj = Solution()
+    command = 'G()(al)()()()()GGGGGGGG'
+    return obj.interpret(command)
+
+if __name__ == "__main__":
+    print(main())
