39e25d86d564c94df297e24fb399da5da0345daa
Dhruv Panchal <dhruvpanchal1491@gmail.com>
2022-03-03 21:34:30 +0530

    solution for max balanced strings split

---
+++ python_solutions/scripts/1221_max_balanced_strings.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def balancedStringSplit(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        s = list(s)
+        ans = 0
+        dict_LR = {
+            'L': 0,
+            'R': 0
+        }
+        l_c = 0
+        r_c = 0
+        while s != []:
+            if s[0] == 'L':
+                l_c += 1
+            elif s[0] == 'R':
+                r_c += 1
+            s.pop(0)
+            if 0 < l_c == r_c > 0:
+                ans += 1
+                l_c = 0
+                r_c = 0
+            # dict_LR[s.pop(0)] += 1
+            # if dict_LR['L'] == dict_LR['R'] > 0:
+            #     ans += 1
+            #     dict_LR = {
+            #         'L': 0,
+            #         'R': 0
+            #     }
+        return ans
+
+
+def main():
+    obj = Solution()
+    s = 'LLLRRRLRLLRR'
+    return obj.balancedStringSplit(s)
+
+if __name__ == "__main__":
+    print(main())
