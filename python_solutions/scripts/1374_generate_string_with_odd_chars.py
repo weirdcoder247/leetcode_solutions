4b36dd6677040368ebaccf2b08d97fd19bc27254
Dhruv Panchal <dhruvpanchal1491@gmail.com>
2022-03-27 13:48:27 +0530

    generate string with odd chars

---
+++ python_solutions/scripts/1374_generate_string_with_odd_chars.py
@@ -0,0 +1,20 @@
+from random import randint
+
+class Solution(object):
+    def generateTheString(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        if n % 2 == 1:
+            return "a" * n
+        else:
+            return "a" * (n - 1) + "b"
+
+def main():
+    n = 4
+    obj = Solution()
+    return obj.generateTheString(n)
+
+if __name__ == '__main__':
+    print(main())
