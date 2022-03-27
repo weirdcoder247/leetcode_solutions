e448dabcc0eac7a08721162fd20d478fd07babb6
Dhruv Panchal <dhruvpanchal1491@gmail.com>
2022-03-27 17:33:33 +0530

    pep8 changesg

--- python_solutions/scripts/1534_count_good_triplets.py
+++ python_solutions/scripts/1534_count_good_triplets.py
@@ -12,16 +12,19 @@ class Solution(object):
         for i in range(0, arr_len - 2):
             for j in range(i + 1, arr_len - 1):
                 for k in range(j + 1, arr_len):
-                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                    if abs(arr[i] - arr[j]) <= a\
+                        and abs(arr[j] - arr[k]) <= b\
+                            and abs(arr[i] - arr[k]) <= c:
                         counter += 1
         return counter
 
+
 def main():
-    arr = [3,0,1,1,9,7]
+    arr = [3, 0, 1, 1, 9, 7]
     a = 7
     b = 2
     c = 3
-    # arr = [1,1,2,2,3]
+    # arr = [1, 1, 2, 2, 3]
     # a = 0
     # b = 0
     # c = 1
