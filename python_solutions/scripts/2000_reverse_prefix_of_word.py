90a232a11bd10a23ceff0c4f0151f48e75d9c8db
Dhruv Panchal <dhruvpanchal1491@gmail.com>
2022-03-27 16:36:42 +0530

    reverse prefix of word

---
+++ python_solutions/scripts/2000_reverse_prefix_of_word.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def reversePrefix(self, word, ch):
+        """
+        :type word: str
+        :type ch: str
+        :rtype: str
+        """
+        ch_index = word.find(ch)
+        if ch_index is not -1:
+            return word[ch_index::-1] + word[ch_index + 1::]
+        return word
+
+def main():
+    word = "abcdefd"
+    ch = "d"
+    obj = Solution()
+    return obj.reversePrefix(word, ch)
+
+if __name__ == "__main__":
+    print(main())
