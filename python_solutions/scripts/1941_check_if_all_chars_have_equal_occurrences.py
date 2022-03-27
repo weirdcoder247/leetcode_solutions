6849751c3258003df189bca3ad1d9ef53f825073
Dhruv Panchal <dhruvpanchal1491@gmail.com>
2022-03-27 14:19:29 +0530

    check if all chars have equal occurrences

---
+++ python_solutions/scripts/1941_check_if_all_chars_have_equal_occurrences.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def areOccurrencesEqual(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        char_dict = dict.fromkeys(set(s), 0)
+        for i in s:
+            char_dict[i] += 1
+
+        return len(set(char_dict.values())) is 1
+
+def main():
+    s = "abacbc"
+    obj = Solution()
+    return obj.areOccurrencesEqual(s)
+
+if __name__ == '__main__':
+    print(main())
