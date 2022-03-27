12423810a4020b4b52b6dae265862b8a3bc384f5
weirdcoder247 <80109389+weirdcoder247@users.noreply.github.com>
2022-03-28 00:45:53 +0530

    Update 1873_calculate_bonus.sql

--- scripts/sql/1873_calculate_bonus.sql
+++ scripts/sql/1873_calculate_bonus.sql
@@ -2,7 +2,8 @@ select a.employee_id
     , coalesce(bonus, 0) as bonus
 from Employees a
 left join
-    (select employee_id, salary as bonus
+    (select employee_id
+        , salary as bonus
     from Employees
     where left(name, 1) != 'M'
         and employee_id % 2 = 1) b
