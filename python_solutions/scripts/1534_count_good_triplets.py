4f03cebb254e3c83e838578109789a26342d5306
Dhruv Panchal <dhruvpanchal1491@gmail.com>
2022-03-27 17:29:21 +0530

    count good triplets

---
+++ python_solutions/scripts/1534_count_good_triplets.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def countGoodTriplets(self, arr, a, b, c):
+        """
+        :type arr: List[int]
+        :type a: int
+        :type b: int
+        :type c: int
+        :rtype: int
+        """
+        i, j, k = 0, 1, 2
+        arr_len = len(arr)
+        counter = 0
+        for i in range(0, arr_len - 2):
+            for j in range(i + 1, arr_len - 1):
+                for k in range(j + 1, arr_len):
+                    if abs(arr[i] - arr[j]) <= a and abs(arr[j] - arr[k]) <= b and abs(arr[i] - arr[k]) <= c:
+                        counter += 1
+        return counter
+
+def main():
+    arr = [3,0,1,1,9,7]
+    a = 7
+    b = 2
+    c = 3
+    arr = [1,1,2,2,3]
+    a = 0
+    b = 0
+    c = 1
+    obj = Solution()
+    return obj.countGoodTriplets(arr, a, b, c)
+
+if __name__ == '__main__':
+    print(main())
