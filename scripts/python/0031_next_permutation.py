3225d3796049e88c6f53fdf4d7c1facc865dad40
charon <charon@kali.www.example.com>
2022-04-03 10:30:00 +0530

    added new file and removed test file

---
+++ scripts/python/0031_next_permutation.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
+def main():
+    pass
+
+if __name__ == "__main__":
+    print(main())
