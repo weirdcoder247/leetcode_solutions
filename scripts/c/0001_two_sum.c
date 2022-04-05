af419e6ff06f58963b80e9f65d4548dc90fe7be6
charon <charon@kali.example.com>
2022-04-05 17:14:02 +0530

    new c solutions

---
+++ scripts/c/0001_two_sum.c
@@ -0,0 +1,14 @@
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+*returnSize=2;
+int *arr=(int *)malloc((*returnSize)*sizeof(int));
+for(int i=0;i<numsSize;i++){
+for(int j=i+1;j<numsSize;j++){
+if(nums[i]+nums[j]==target){
+arr[0]=i;
+arr[1]=j;
+break;
+}
+}
+}
+return arr;
+}
--- scripts/c/2000_reverse_prefix_of_word.c
+++ scripts/c/2000_reverse_prefix_of_word.c
@@ -4,10 +4,10 @@ void moo(){
     /* do something and don't return a value */
     printf("Print Successful!!!");
     return;
-};
+}
 
-unsigned int main(){
+int main(__attribute__((unused)) int argc, const char *argv[]){
     moo();
     printf("\nPrint Success in Main as well!!!");
     return 0;
-};
+}
