37589a551b04faafdbccc3feaa745788564f5e36
charon <charon@pop-os.localdomain>
2022-04-27 19:00:11 +0530

    solution to find target node in clone of a binary tree

---
+++ scripts/python/1379_find_target_node_in_clone_tree.py
@@ -0,0 +1,113 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x=0):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def stringToTreeNode(self, input):
+        input = input.strip()
+        input = input[1:-1]
+
+        inputValues = [s.strip() for s in input.split(',')]
+        root = TreeNode(int(inputValues[0]))
+        nodeQueue = [root]
+        front = 0
+        index = 1
+        while index < len(inputValues):
+            node = nodeQueue[front]
+            front = front + 1
+
+            item = inputValues[index]
+            index = index + 1
+            if item != "null":
+                leftNumber = int(item)
+                node.left = TreeNode(leftNumber)
+                nodeQueue.append(node.left)
+
+            if index >= len(inputValues):
+                break
+
+            item = inputValues[index]
+            index = index + 1
+            if item != "null":
+                rightNumber = int(item)
+                node.right = TreeNode(rightNumber)
+                nodeQueue.append(node.right)
+        return root
+
+    def treeNodeToString(self, root):
+        if not root:
+            return "[]"
+        output = ""
+        queue = [root]
+        current = 0
+        while current != len(queue):
+            node = queue[current]
+            current = current + 1
+
+            if not node:
+                output += "null, "
+                continue
+
+            output += str(node.val) + ", "
+            queue.append(node.left)
+            queue.append(node.right)
+        return "[" + output[:-2] + "]"
+
+    def prettyPrintTree(self, node, prefix="", isLeft=True):
+
+        if not node:
+            print("Empty Tree")
+            return
+
+        if node.right:
+            self.prettyPrintTree(node.right, prefix + ("│   " if isLeft else "    "), False)
+
+        print(prefix + ("└── " if isLeft else "┌── ") + str(node.val))
+
+        if node.left:
+            self.prettyPrintTree(node.left, prefix + ("    " if isLeft else "│   "), True)
+
+    def getTargetCopy(self, original: TreeNode, cloned: TreeNode, target: TreeNode) -> TreeNode:
+        return self.findTargetInClone(target, cloned)
+
+    def findTargetInClone(self, target: TreeNode, root: TreeNode) -> TreeNode:
+        self.target_node = None
+        def findTN(curr_node):
+            if target.val == curr_node.val:
+                self.target_node = curr_node
+            if curr_node.left:
+                findTN(curr_node.left)
+            if curr_node.right:
+                findTN(curr_node.right)
+        findTN(root)
+        return self.target_node
+
+
+    def findTargetNode(self, target: int, root: TreeNode) -> TreeNode:
+        self.target_node = None
+        def findTN(curr_node):
+            if target == curr_node.val:
+                self.target_node = curr_node
+            if curr_node.left:
+                findTN(curr_node.left)
+            if curr_node.right:
+                findTN(curr_node.right)
+        findTN(root)
+        return self.target_node
+
+def main():
+    tree = '[7,4,3,null,null,6,19]'
+    target = 3
+    obj = Solution()
+    original = obj.stringToTreeNode(tree)
+    cloned = obj.stringToTreeNode(tree)
+    target_node = obj.findTargetNode(target, original)
+    return obj.getTargetCopy(original, cloned, target_node)
+
+if __name__ == "__main__":
+    print(main())
+
---
+++ scripts/python/2236_root_equals_sum_of_children.py
@@ -0,0 +1,86 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+class Solution:
+    def stringToTreeNode(self, input):
+        input = input.strip()
+        input = input[1:-1]
+
+        inputValues = [s.strip() for s in input.split(',')]
+        root = TreeNode(int(inputValues[0]))
+        nodeQueue = [root]
+        front = 0
+        index = 1
+        while index < len(inputValues):
+            node = nodeQueue[front]
+            front = front + 1
+
+            item = inputValues[index]
+            index = index + 1
+            if item != "null":
+                leftNumber = int(item)
+                node.left = TreeNode(leftNumber)
+                nodeQueue.append(node.left)
+
+            if index >= len(inputValues):
+                break
+
+            item = inputValues[index]
+            index = index + 1
+            if item != "null":
+                rightNumber = int(item)
+                node.right = TreeNode(rightNumber)
+                nodeQueue.append(node.right)
+        return root
+
+    def treeNodeToString(self, root):
+        if not root:
+            return "[]"
+        output = ""
+        queue = [root]
+        current = 0
+        while current != len(queue):
+            node = queue[current]
+            current = current + 1
+
+            if not node:
+                output += "null, "
+                continue
+
+            output += str(node.val) + ", "
+            queue.append(node.left)
+            queue.append(node.right)
+        return "[" + output[:-2] + "]"
+
+    def prettyPrintTree(self, node, prefix="", isLeft=True):
+
+        if not node:
+            print("Empty Tree")
+            return
+
+        if node.right:
+            self.prettyPrintTree(node.right, prefix + ("│   " if isLeft else "    "), False)
+
+        print(prefix + ("└── " if isLeft else "┌── ") + str(node.val))
+
+        if node.left:
+            self.prettyPrintTree(node.left, prefix + ("    " if isLeft else "│   "), True)
+
+    def checkTree(self, root: Optional[TreeNode]) -> bool:
+        return root.val == root.left.val + root.right.val
+
+def main():
+    obj = Solution()
+    root = '[10,4,6]'
+    root = obj.stringToTreeNode(root)
+    return obj.checkTree(root)
+
+if __name__ == "__main__":
+    print(main())
+
